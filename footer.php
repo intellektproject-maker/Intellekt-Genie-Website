3fb34d13215c096f9d770b564680853644c8fb46
Intellekt_Academy <intellektproject@gmail.com>
2026-09-18 16:39:17 +0530

    Restore Popper integrity while keeping Company Profile slideshow

--- footer.php
+++ footer.php
@@ -83,7 +83,7 @@
   <script src="assets/js/main.js"></script>
   <script src="assets/js/company-tabs.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvR+Vc4jQkC+hVqc2pM8ODewa9r"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
       crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
       integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK"
