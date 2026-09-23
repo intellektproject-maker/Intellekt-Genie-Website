6b4e4793986ba5ef5311011dea7513f2f806c67f
Intellekt_Academy <intellektproject@gmail.com>
2026-09-23 17:29:32 +0530

    Show footer only on homepage

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
   <button id="scrollToTopBtn" title="Go to top" class=" fs-5 "><i class="fas fa-arrow-up"></i></button>
 
+  <?php if (basename($_SERVER['SCRIPT_NAME']) === 'index.php'): ?>
+
   <footer class="container-fluid" id="footer">
       <div class="container">
           <div class="row pt-5">
@@ -84,3 +86,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
       integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK"
       crossorigin="anonymous"></script>
+
+  <?php endif; ?>
