9b9ee5da2fd4d38be0f0a9eb240e9d8d8d76231a
Intellekt_Academy <intellektproject@gmail.com>
2026-09-23 17:10:33 +0530

    Harden contact form session cookie settings

--- contact-us.php
+++ contact-us.php
@@ -1,4 +1,9 @@
 <?php
+session_set_cookie_params([
+    'httponly' => true,
+    'secure' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'),
+    'samesite' => 'Lax',
+]);
 session_start();
 
 if (empty($_SESSION['contact_csrf'])) {
